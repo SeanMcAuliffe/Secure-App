22c736c8413de4bbaa1e45d0508be0f78a85a51e
SeanMcAuliffe <seanmcauliffe@uvic.ca>
2022-11-25 22:55:47 -0800

    Add comments to required encryption functions

--- tcp_client/encryption.py
+++ tcp_client/encryption.py
@@ -9,45 +9,57 @@ from cryptography.fernet import Fernet
 # ------------------------------------------------------------------------------
 def generate_static_keypair(username, password):
     """ Generates a new RSA keypair and saves it to disk, under the username
-    and password provided."""
+    and password provided. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 def load_static_keypair(username, password):
+    """ Loads a keypair from disk, using the username and password provided,
+    to be loaded by the TerminalClient upon user authentication. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 def generate_session_keypair():
+    """ Generates a new asymmetric RSA keypair for use in a single session
+    establishment ritual, does not save the keypair to disk. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-def generate_session_key():
+def generate_symmetric_session_key():
+    """ Generates a single-use shared key (symmetric, using Fernet) for
+    use in a single message exhange. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 def encrypt_message(message, key):
+    """ Encryptes a message under the provided key using AES-256. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
 def decrypt_message(message, key):
+    """ Decryptes a message under the provided key using AES-256. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-def sign_message(message, private_key):
+def sign_message(message, shared_session_key):
+    """ Generates a hash of the key to sign the provided message. """
     pass
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-def verify_signature(message, signature, public_key):
+def verify_signature(message, signature, shared_session_key):
+    """ Verifies that the provided signature matches the hash of the
+    shared key. """
     pass
 # ------------------------------------------------------------------------------
 
+# Below may be redundant, just use the genereic encrypt/decrpyt msg functions
 # ------------------------------------------------------------------------------
 def encrypt_session_key(session_key, recipient_public_key):
     pass
